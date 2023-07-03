0d1eee4de1a7b096a4bd120982d5faeb98a267f8
pedrotnascimento@gmail.com <pedrotnascimento@gmail.com>
2023-07-03 00:29:39 -0300

    strucutuing page

--- src/components/templates/GoalSettingTemplate.tsx
+++ src/components/templates/GoalSettingTemplate.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./goalSettingTemplateStyle.scss";
 
 export function GoalSettingTemplate() {
     const [goal, setGoal] = useState("");
@@ -12,15 +13,15 @@ export function GoalSettingTemplate() {
             <h2 className="template-title">Goal Setting Template</h2>
         </div>
         <div className="goal-setting-template-content">
-            <div className="header">
-                <div className="goal-content lane-left">
+            <div className="goal-header">
+                <div className="lane lane-left">
                     <div className="goal-header">What is your Goal?</div>
                     <div>
                         <input onChange={(e) => setGoal(e.target.value)}
                             placeholder="Write Goal" />
                     </div>
                 </div>
-                <div className="important-content lane-right">
+                <div className="lane lane-right">
                     <div className="important-header">Why is it Important?</div>
                     <div>
                         <input onChange={(e) => setImportance(e.target.value)}
@@ -29,51 +30,59 @@ export function GoalSettingTemplate() {
                 </div>
             </div>
             <div className="smart-topic">
-                <div>Is your goal SMART?</div>
-                <div>
+                <div className="smart-letter-content">
+                    Is your goal SMART?</div>
+                <div className="smart-letter-content">
                     <div className="smart-letter">S</div>
-                    <div>
-                        <div>
-                            Is it specific? <input type="checkbox" />
-                        </div>
-                    </div>
-                    <div className="smart-letter">M</div>
-                    <div>
-                        <div>
-                            Is it measureable? <input type="checkbox" />
-                        </div>
-                    </div>
-                    <div className="smart-letter">A</div>
-                    <div>
-                        <div>
-                            Is it attainable? <input type="checkbox" />
-                        </div>
-                    </div>
-                    <div className="smart-letter">R</div>
-                    <div>
-                        <div>
-                            Is it relevant? <input type="checkbox" />
-                        </div>
-                    </div>
-                    <div className="smart-letter">T</div>
-                    <div>
-                        <div>
-                            Is it time-specific? <input type="checkbox" />
-                        </div>
+                    <div className="smart-letter-description">
+                        Is it specific? <input type="checkbox" />
                     </div>
                 </div>
+                <div className="smart-letter-content">
+                    <div className="smart-letter">M</div>
+
+                    <div className="smart-letter-description">
+                        Is it measureable? <input type="checkbox" />
+                    </div>
+
+                </div>
+                <div className="smart-letter-content">
+                    <div className="smart-letter">A</div>
+
+                    <div className="smart-letter-description">
+                        Is it attainable? <input type="checkbox" />
+                    </div>
+
+                </div>
+                <div className="smart-letter-content">
+                    <div className="smart-letter">R</div>
+
+                    <div className="smart-letter-description">
+                        Is it relevant? <input type="checkbox" />
+                    </div>
+
+                </div>
+                <div className="smart-letter-content">
+                    <div className="smart-letter">T</div>
+
+                    <div className="smart-letter-description">
+                        Is it time-specific? <input type="checkbox" />
+                    </div>
+
+                </div>
+
             </div>
             <div className="reason">
-                <textarea placeholder="Say the reasons">
+                <textarea cols={50} rows={5} placeholder="Say the reasons for ">
                 </textarea>
             </div>
             <div className="goal-study">
-                <div className="potetial-blockers">
+                <div className="lane">
                     <div className="goal-study__header">Potential Blockers</div>
                     <div className="goal-study__body">{potentialBlockerDescription}</div>
 
                 </div>
-                <div className="action-plan">
+                <div className="lane">
                     <div className="goal-study__header">Action Plan</div>
                     <div className="goal-study__body">
                         {actionPlanDescription}
